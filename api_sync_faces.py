08dbeb9c31a6e21295cf59cb87962a230be62aa9
octasanchez21 <granados@inteliksa.com>
2025-04-01 16:31:25 -0300

    api

--- api_sync_faces.py
+++ api_sync_faces.py
@@ -15,7 +15,8 @@ def handle_sync():
     return {"message": "✅ Rostros sincronizados correctamente"}, 200
 
 if __name__ == "__main__":
-    init_db()  # Asegura que la base de datos esté lista
-import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    init_db()
+    app.run(host="0.0.0.0", port=port)
+
